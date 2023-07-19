ec1f4afac0c7957529a06ba9d2e697b3125b5c2d
Abu Rayhan Siddike <aburayhansiddike@gmail.com>
2023-07-19 10:47:56 +0600

    All actions url added done

--- CatApp/urls.py
+++ CatApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from CatApp import views
 
 urlpatterns = [
-
+    path('cat/', views.CatList.as_view(), name="catList"),
+    path('cat/<int:pk>', views.CatDetails.as_view(), name="catDetails")
 ]
