a11fba3c1cbab67114f6b57aff3832c7df981eb2
Abu Rayhan Siddike <aburayhansiddike@gmail.com>
2023-07-19 10:18:04 +0600

    CatShop models and its necessary fields added done

--- CatApp/admin.py
+++ CatApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from CatApp.models import CatShop
 # Register your models here.
+
+
+admin.site.register(CatShop)
--- CatApp/models.py
+++ CatApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CatShop(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    breed = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
