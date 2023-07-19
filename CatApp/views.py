47c44f1391ca7f01fd1adcc42024e133de9fa08b
Abu Rayhan Siddike <aburayhansiddike@gmail.com>
2023-07-19 10:39:29 +0600

    CRUD operations in views is done

--- CatApp/views.py
+++ CatApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from CatApp.models import CatShop
+from CatApp.serializers import CatShopSerializer
+
 
 # Create your views here.
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = CatShop.objects.all()
+    serializer_class = CatShopSerializer
+
+
+class CatDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CatShop.objects.all()
+    serializer_class = CatShopSerializer
+
